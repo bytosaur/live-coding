a7adf43b5edd4bae6a23d090a36ec9078867e733
bytosaur <bethgeph@gmail.com>
2024-02-14 16:30:51 +0800

    small update

--- hydra/concerts/IOC23/full_set.js
+++ hydra/concerts/IOC23/full_set.js
@@ -1,76 +1,64 @@
 // licensed with CC BY-NC-SA 4.0
 // https://creativecommons.org/licenses/by-nc-sa/4.0/
-// Hydra by Olivia Jack
-// https://github.com/hydra-synth/hydra-synth
+// vapor wave collection
+// paul henri
 
-bpm=20
-//heart
-shape(3)
-  	.add(shape(99).scroll(-0.125,0.2))
-	.add(shape(99).scroll(0.125,0.2))
-	.color(() => a.fft[2]*2,0.5,.6)
-	.pixelate(50,50)
-	.scale(0.8,1.1,1.4)
-	.modulate(osc().rotate(Math.PI/2),0.01)
-  	.out(o0)
-render(o0)
+a.show()
 
-//heartrain
-src(o0)
-	.mask(noise(100,0.5).posterize(3,1).pixelate(512,512).modulate(noise(10),[-0.2,0.2].ease()).brightness([-1,1].ease()))
-	.out(o1)
-render(o1)
-
-
-// trail
-src(o2)
-  .scrollX(()=>(Math.cos(time/2)/100))
-  .scrollY(()=>(time%1 < 5 ? 0.01 : 0.2))
-  .scale(0.99)
-  .hue(.016)
-  // .color(0.9, 0.99, 0.99)
-  .layer(o1)
-  .out(o2)
-render(o2)
+// $$$ COLORS $$$
+// orange: 1,0.6,0.4
 
 
 // $$$ OBJECTS $$$ \\
 
 // triforce
-triforce = () => shape(3, 0.4, 0)
+triforce = () => shape(3, 0.4, 0.001)
 	.color(1, 1, 0)
 	.rotate(Math.PI)
 	.sub(shape(3, 0.2))
 	// .scale(() => 2 * Math.exp(time % 8 / 8) - 1)
 
-// sun
-sun = () => shape(99, 0.2, 0).color(1, 0.6, 0.4)
+// heart
+heart = () => shape(3)
+  	.add(shape(99).scroll(-0.125,0.2))
+	.add(shape(99).scroll(0.125,0.2))
+	.scale(0.8,1.1,1.4)
 
-sun_and_moon = () =>
+// drink
+fruit = () => shape(3).scroll(0.33,0.33).kaleid(8).mask(shape(99,0.5))
+
+drink = () => fruit().scale(0.6).rotate(1,1).scroll(0.21,0.32)
+  .layer(shape(3,0.25,0).scrollY(0.2).r())
+  .layer(shape(4,0.4,0).scale(1,0.1,1).scrollY(-0.2).r())
+  .layer(shape(3,0.1,0).scale(1,1,-1).scrollY(-0.4).r())
+
+// sun and moon
+sun_and_moon = (speed=0.4) =>
     solid()
     .layer(
       shape(99).scale(1,1)  // sun
       .mask(gradient().rotate(Math.PI).pixelate(1,8).thresh(0.4,0.2))
-          .scrollX(()=>(Math.sin(time)/3))
-          .scrollY(()=>(Math.cos(time)/3))
+          .scrollX(()=>(Math.sin(time*speed)/3))
+          .scrollY(()=>(Math.cos(time*speed)/3))
             .r()
       )
     .layer(
       shape(99).scale(1,1).mask(shape(99).scrollX(0.05).invert()) // moon
       .mask( gradient().rotate(Math.PI).pixelate(1,8).thresh(0.4,0.2))
-          .scrollX(()=>(-Math.sin(time)/3))
-          .scrollY(()=>(-Math.cos(time)/3))
+          .scrollX(()=>(-Math.sin(time*speed)/3))
+          .scrollY(()=>(-Math.cos(time*speed)/3))
             .r()
       )
 
 // palm
-palm = () => new Array(10).fill().reduce((a,b)=> (
+palm = () => new Array(4).fill().reduce((a,b)=> (
         a.layer(shape(3).r().scale(0.2,1)
                 .rotate(Math.random(),1))), solid())
       .scale(1.5).scrollY(0.27).color(0,1,1)	// leafs
       .add(osc(100,0.03,1).rotate(-Math.PI/2).mask(  // trunk
               shape(4).scale(0.2,8).rotate(Math.PI/2)))
-.luma()
+
+palm().scale(1,3,1).repeat(4,1).out(o2)
 
 // birds
 birds = () => new Array(4).fill().reduce(
@@ -88,134 +76,67 @@ birds = () => new Array(4).fill().reduce(
   ), solid()
 )
 
-// clouds
-cloud0_o = (o) => new Array(20).fill().reduce(
-	(a,b,i) => (
-      a.layer(
-        shape(99, Math.random()/8 + 0.02, 0.000002)
-        .scroll(Math.random()/10+Math.random()/5*(i%3),
-              	Math.random()/30+Math.random()/20*(i%3) + 0.2)
-        .r()
-      )
-    ), solid())
-  .modulate(noise(10).color(1,0), 0.02)
-  .scrollX(()=>(time)/40)
-  .r()
-  //.mult(osc(1,0.2,2)).colorama()
- .out(o)
-
-cloud1_o = (o) => new Array(30).fill().reduce(
- 	(a,b,i) => (
-       a.layer(
-         shape(99, Math.random()/15 + 0.02, 0.000001)
-         .scroll((i%3)/3 + 0.25,	0.22)
-				 .scroll(Math.random()/4, Math.random()/20)
-        // .scroll(Math.random()/10+Math.random()/5*(i%3),
-        //       	Math.random()/30+Math.random()/20*(i%3))
-         .r()
-       )
-     ), solid())
-  	.scrollX(()=>(time)/40)
-	.r()
-	//.mult(osc(1,0.2,2)).colorama()
-.out(o)
-
 
 // $$$ FLOORS $$$ \\
 
-floor = () => gradient(1)
-  	.diff(shape(4,0.2).scale(8, .1).repeat(10,1)
-  		.layer(shape(4,0.2).scale(1, 8).repeat(1,10).r())
-    .scrollY(()=>(-time/2))
-  	.modulateScale(gradient().brightness(-0.9).g()).scrollY(0.6))
-  	.mask(gradient().rotate(Math.PI).g().thresh(0.5,0).invert())
-
+grid = (size=0.9, repeat=4) => shape(4,size,0).repeat(repeat,repeat)
 
 water = () => solid(0.3,0.3,1)
-  .layer(solid()
-    .layer(shape(4).scale(1,0.3,10).r())
-    .layer(shape(4).scale(1,0.3,10).rotate(Math.PI/2).r())
-    .repeat(4,4)
-    .scroll(0.02,0)
-  	.color(0.2,0.2,0.2)
-    .r()
-   )
-  .layer(solid()
-    .layer(shape(4).scale(1,0.2,10).r())
-    .layer(shape(4).scale(1,0.2,10).rotate(Math.PI/2).r())
-    .repeat(4,4)
-    .r()
-   )
+  .layer(grid(0.9, 8).scroll(0.02,0).color(0.2,0.2,0.2).r())
+  .layer(grid(0.9, 8).invert().r())
   .modulate(noise(5,0.1), 0.1)
   .scrollY(()=>(Math.sin(time))/40)
-  .modulateScale(gradient().g().brightness(-1))
-  .scrollY(0.5)
-  //.add(gradient().g().brightness(-1),-0.9)
+  .modulateScale(gradient().brightness(-0.95).g(),1)
+  .scrollY(0.6)
   .mask(gradient().g().thresh(0.5,0))
 
-water().out()
-
+floor = (size=0.9, repeat=8, speed=0.2) =>
+	grid(size, repeat, speed)
+    .scrollY(()=>(-time*speed))
+  	.modulateScale(gradient().brightness(-0.9).g(),1).scrollY(0.6)
+  	.mask(gradient().g().thresh(0.5,0))
 
 // $$$ BACKGROUNDS $$$ \\
 
-back_grad = () => gradient().g().invert().color(1,0.6,0.6).brightness(0.3)
-
 back_colo = () => osc(3,0,0.4).colorama([0.97,1].ease().fast(0.15)).rotate(Math.PI/2)
 
-back_colo2 = () => gradient().colorama(()=>(Math.sin(time)+1)/10)
+
+// $$$ TESTING $$$ \\
+
+back_colo()
+	.layer(sun_and_moon().color(1,0.1, 0).colorama().luma(0.01))
+	.layer(water().modulate(osc(10,0.1).rotate(Math.PI/2),0.04))
+	.out(o1)
+
+floor2()
+//.layer(water().mask(osc().thresh()))
+.layer(birds().scrollY(-0.25).modulate(noise(),0.02).mult(gradient()).colorama(2).scale(4).luma(0.2,0))
+.out(o1)
 
 
 
-// $$$ SKYS $$$ \\
-
-
-sky_clouds_i = (i) => solid(0.3,0.3,1)
-  .layer(src(i).scrollX(Math.random()).scale(2).scrollY(0.2))
-  .layer(src(i).scrollX(Math.random()).scale(1.5).scrollY(0.1))
-  .layer(src(i).scrollX(Math.random()))
-  //.layer(src(o0).scrollX(Math.random()).scale(0.25).scrollY())
-  .mask(gradient().g().invert().thresh(0.5,0))
-
-
-sky_storm_i = (i) => solid(0.3,0.3,1)
-  .layer(noise(100,10).thresh(0.7,0).pixelate(1,1).r())
-  .layer(src(i).scrollX(Math.random()).scale(1.5))
-  .layer(src(i).scrollX(Math.random()))
-  .layer(src(i).scrollX(Math.random()).scale(0.7))
-  .layer(src(i).scrollX(Math.random()).scale(0.45).scrollY(-0.05))
-  .mask(gradient().g().invert().thresh(0.5,0))
-  .pixelate(64,64)
-  .brightness(-0.5)
-
-
-// writes to o0
-cloud0_o(o1)
-
-cloud1_o(o0)
-
-// reads o0 writes to o1
-sky_clouds_i(o0).out(o1)
-
-sky_storm_i(o0).out(o1)
-
-
-
-
-
-// Effects
+// $$$ EFFECTS $$$
 
 // buffer freeze
-src(o0)
+src(o2)
 .scrollX(0.001)
-.modulate(o0, [0.001, -0.003])
-.out(o0)
+//.blend(src(o1).colorama(()=>(cc[2])),()=>(cc[1]))
+//.blend(src(o1).pixelate(180,200).colorama(2), 0.4)
+.hue(0.005)
+.luma(0.02,0)
+.modulate(o2, [0.001, -0.003])
+.out(o2)
 
+render()
 
 // 3d effect
 src(o3)
-.scale(0.99)
+.brightness(-0.001)
+.rotate(0.002)
+.scale(1, 1.001, 1.002)
 .layer(
-  src(o1)
-  .luma()
+  floor().mask(drink())
   )
 .out(o3)
+
+render(o3)
